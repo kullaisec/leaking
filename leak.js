8b97b8c0656c830ddc7a1217045c08fcc7cf9195
Kullai Metikala <99985908+kullaisec@users.noreply.github.com>
2025-06-04 20:48:50 +0530

    Create leak.js

---
+++ leak.js
@@ -0,0 +1,6 @@
+ }
+    bitlyUrl = 'https://api-ssl.bitly.com/v3/shorten';
+    accessToken = '837c0c4f99fcfbaca55ef9073726ef1f6a5c9349';
+    @observable loading = false;
+    @observable urlGenerated = false;
+}
