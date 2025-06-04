2edbcfa6289aaf5b0940cf9256dac6f47b1892a5
Kullai Metikala <99985908+kullaisec@users.noreply.github.com>
2025-06-04 20:53:33 +0530

    Create alogia.js

---
+++ alogia.js
@@ -0,0 +1,7 @@
+ algolia: {
+        appId: "HUSVJ6LWVI",
+        apiKey: "39dddd93a49e7336d5bc18024ac8a5cd",
+        indexName: "linea-docs",
+        contextualSearch: true,
+        insights: true,
+      },
